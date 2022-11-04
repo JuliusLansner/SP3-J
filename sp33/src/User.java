6b6aff8b85fa3407e0610ba67a75b4871ff550b3
Sebastian_cph681 <CbEdelhardt@gmail.com>
2022-11-04 11:10:45 +0100

    Unused methods removed

--- sp33/src/User.java
+++ sp33/src/User.java
@@ -5,59 +5,20 @@ public class User {
     Scanner input = new Scanner(System.in);
     String userName;
     String passWord;
-    String UP;
 
-    public String userLogin(String userName, String passWord) {
-
+    public void userLogin(String userName, String passWord) {
             this.userName = userName;
             this.passWord = passWord;
-
-            return ":)";
         }
 
     //---- getter, user login information ----
     public String getUserName() {
+        userName = input.nextLine();
         return userName;
     }
     public String getPassWord() {
+        passWord = input.nextLine();
         return passWord;
     }
 
-    //---- user login / register ----
-    public String getUserLogin () {
-        System.out.println("You are about to login to your account. Please write the following ..");
-        System.out.println("Username:   ");
-        userName = input.nextLine();
-        System.out.println("Password:   ");
-        passWord = input.nextLine();
-
-        return userName + passWord;
-    }
-    private String userRegister () {
-        System.out.println("You are about to register a new account. Please write the following ..");
-        System.out.println("Username:   ");
-        userName = input.nextLine();
-        System.out.println("Password:   ");
-        passWord = input.nextLine();
-
-        return userName + passWord;
-    }
-
-
-            /*
-            private String getUserRegister () {
-                System.out.println("You are about to register a new account. Please write your desired ..");
-                System.out.println("Username:   ");
-                userName = input.nextLine();
-                System.out.println("Password:   ");
-                passWord = input.nextLine();
-                UP = userName + passWord;
-                return UP;
-
-
-            }
-            public String getLogin () {
-                return UP;
-
-             */
 }
