4b373ae2c61832149f2211b285d17b08c7dedf72
cph-mh1088 <112551104+cph-mh1088@users.noreply.github.com>
2022-11-04 09:04:21 +0100

    Small changes and diagram
    Small changes. Added a sequence diagram for the class

--- sp33/src/StartMenu.java
+++ sp33/src/StartMenu.java
@@ -6,10 +6,9 @@ public class StartMenu {
     public static int loginScreen() {
         System.out.println("Welcome to Jflix");
         System.out.println("Please choose an existing user, or create a new one");
-        System.out.println("1 - login to user 1");
-        System.out.println("2 - create a new user");
+        System.out.println("Login to user 1: Press 1");
+        System.out.println("Create a new user: Press 2");
         System.out.println("Enter your choice: ");
-
         String answer;
         while (true) {
             answer = scanner.nextLine();
@@ -22,18 +21,15 @@ public class StartMenu {
             System.out.println("You must choose option 1 or 2. Try again");
         }
     }
-    public static void loginExistingUser (User user){
+    public static void loginExistingUser(User user){
         if (user.equals(existingUser)){
             //display main menu?
-        } else {
-            return;
         }
     }
     public static void newUser(User user){
         if (user.equals(newUser)){
             //display create a new profile
-        } else {
-            return;
+            //refer to createNewProfileMethod
         }
     }
 }
@@ -41,7 +37,7 @@ public class StartMenu {
 /*
 WHAT DOES THE STARTMENU CLASS NEED TO DO?
 1. Print a welcome/start menu - DONE
-2. Take a user input and process it. Either Log in to existing profile or create a new profile.
+2. Take a user input and process it. Either Log in to existing profile or create a new profile. - DONE? DOES IT WORK?
 3. If a user chooses an existing profile display MainMenu
 4. If a user chooses to create a new profile, display NewUser
 */
