fd1c8e2f25d93fd4186e481a6a0b4717b33d484e
Malde <cph-mm737@cphbusiness.dk>
2022-11-06 15:45:47 +0100

    redoneagain

--- sp33/src/Main.java
+++ sp33/src/Main.java
@@ -5,14 +5,18 @@ import java.util.ArrayList;
 public class Main {
 
 
-
-
     public static void main(String[] args) {
 
-JflixDB test = new JflixDB();
-//System.out.println(test.createMovieList(test.getMovies()));
-test.createMovieList(test.getMovies());
+
+        MainMenu test1 = new MainMenu();
+
+        //System.out.println(test1.searchMovie());
+        //System.out.println(test1.searchYear());
+        //System.out.println(test1.searchgenre());
+
 
 
     }
+
+
 }
